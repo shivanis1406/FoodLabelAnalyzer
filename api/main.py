4d4fd23cad70445e07a11e05c16886078a925d59
shivanis1406 <61353420+shivanis1406@users.noreply.github.com>
2024-11-18 20:18:55 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 
 # Import your individual API apps
 from api.data_extractor import app as data_extractor_app
-from api.another_app import app as another_app
+from api.nutrient_analyzer import app as nutrient_analyzer_app
 
 main_app = FastAPI()
 
 # Mount individual apps
-main_app.mount("/data-extractor", data_extractor_app)
-main_app.mount("/another-app", another_app)
+main_app.mount("/", data_extractor_app)
+main_app.mount("/", nutrient_analyzer_app)
 
 # Optional: Add a root endpoint
 @main_app.get("/")
