5ac512368a26c125d1f44f574c65554be2c6dbf7
shivanis1406 <61353420+shivanis1406@users.noreply.github.com>
2024-11-19 14:07:03 +0530

    Update nutrient_analyzer.py

--- api/nutrient_analyzer.py
+++ api/nutrient_analyzer.py
@@ -3,7 +3,7 @@ import traceback
 import sys
 from functools import wraps
 from .icmr import analyze_nutrients
-from .rda import find_nutrition
+from .rda import find_nutrition, rda_analysis
 import os
 import json
 from fastapi import FastAPI, HTTPException
